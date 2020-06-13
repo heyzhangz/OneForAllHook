05bc19f2975d71b4450645e5c80564a60c39c3d6
blackmax <14041528@hdu.edu.cn>
2020-06-13 22:13:57 +0800

    add request permission target api

--- agent/scenario.ts
+++ agent/scenario.ts
@@ -17,10 +17,16 @@ export function cameraReleatedHook(trace_flag: boolean) {
 export function locationReleatedHook(trace_flag: boolean) {
     let target_classes:string[];
     target_classes = [
-      'android.location.LocationManager', 
-      'android.telephony.TelephonyManager',
+      'android.location.ILocationManager',
+      'android.location.ILocationManager$Stub',
+      'android.location.ILocationManager$Stub$Proxy',
+      'android.location.LocationManager',
       'android.location.LocationManager$GnssStatusListenerTransport$1',
-      'android.location.LocationManager$GnssStatusListenerTransport$GnssHandler'
+      'android.location.LocationManager$GnssStatusListenerTransport$2',
+      'android.location.LocationManager$GnssStatusListenerTransport$Nmea',
+      'android.location.LocationManager$ListenerTransport',
+      'android.location.LocationManager$ListenerTransport$1',
+      'android.location.LocationManager$ListenerTransport$2'
     ]
     target_classes.forEach((clazz) => {
       h.hook_class_methods(clazz, trace_flag);
@@ -48,7 +54,12 @@ export function life_cycle_hook(trace_flag: boolean) {
     'android.app.Service'
   ]
   target_classes.forEach((clazz) => {
-    h.hook_target_methods(clazz, 'onCreate', trace_flag);
+    h.hook_target_methods(clazz, 'onCreate', trace_flag, false);
     // h.hook_class_methods(clazz, false);
   })
+}
+
+export function permission_request_hook(trace_flag: boolean) {
+  h.hook_target_methods('android.app.Activity', 'requestPermissions', trace_flag, true);
+  h.hook_target_methods('android.app.Fragment', 'requestPermissions', trace_flag, true);
 }
